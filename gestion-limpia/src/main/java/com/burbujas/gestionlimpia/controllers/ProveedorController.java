84ca17566e7a23e92f7d0241659564cc9d05df1a
Hernan Frank <hernanvfrank@gmail.com>
2024-02-15 17:36:02 -0300

    -Separo los handlers de cada entidad en diferentes controllers.
    -Arreglo enrutamiento.
    
    -Pongo nombre de la lavandería como variable global.

---
+++ gestion-limpia/src/main/java/com/burbujas/gestionlimpia/controllers/ProveedorController.java
@@ -0,0 +1,99 @@
+package com.burbujas.gestionlimpia.controllers;
+
+import com.burbujas.gestionlimpia.models.entities.Proveedor;
+import com.burbujas.gestionlimpia.models.services.IProductoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/productos/proveedores")
+public class ProveedorController {
+
+    private final IProductoService productoService;
+
+    @Autowired
+    public ProveedorController(IProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+
+    @GetMapping("/listar")
+    public String listarProveedores(Model model){
+        model.addAttribute("titulo", "Proveedores");
+
+        List<Proveedor> proveedores = productoService.findAllProveedores();
+
+        model.addAttribute("proveedores", proveedores);
+
+        return "inventario/proveedores/proveedores";
+    }
+
+    @GetMapping(value = "/nuevo")
+    public String crearProveedor(Model model) {
+        model.addAttribute("titulo", "Nuevo proveedor");
+
+        Proveedor proveedor = new Proveedor();
+
+        model.addAttribute("proveedor", proveedor);
+
+        return "inventario/proveedores/proveedor";
+    }
+
+    @GetMapping(value = "/editar/{id}")
+    public String editarProveedores(@PathVariable(value = "id") Long id, RedirectAttributes flashmsg, Model model) {
+
+        Proveedor proveedor = productoService.findProveedorById(id);
+        if (id > 0 && proveedor != null) {
+
+            model.addAttribute("proveedor", proveedor);
+
+            model.addAttribute("titulo", "Editar proveedor");
+            return "inventario/proveedores/proveedor";
+        } else {
+            flashmsg.addFlashAttribute("danger", "No se encontró el proveedor.");
+            return "redirect:/productos/proveedores/listar";
+        }
+
+    }
+
+    @PostMapping(value = "/guardar")
+    public String guardarProveedor(@Valid Proveedor proveedor, BindingResult result, Model model, RedirectAttributes flashmsg) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", model.getAttribute("titulo"));
+            return "inventario/proveedores/proveedor";
+        }
+
+        productoService.saveProveedor(proveedor);
+
+        flashmsg.addFlashAttribute("success", "Listado de reabastecimientos actualizado");
+
+        return "redirect:/productos/proveedores/listar";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarProveedor(@PathVariable(name = "id") Long id, RedirectAttributes flashmsg){
+        Proveedor proveedor = productoService.findProveedorById(id);
+        if (proveedor != null) {
+            if(proveedor.getReabastecimientos().size() > 0){
+                // antes de borrar el proveedor, borro todos sus reabastecimientos si tiene
+                proveedor.getReabastecimientos().forEach(reabastecimiento -> productoService.deleteReabastecimiento(reabastecimiento.getProducto(),reabastecimiento));
+            }
+            productoService.deleteProveedor(id);
+            flashmsg.addFlashAttribute("success", "Proveedor eliminado correctamente.");
+        }
+        flashmsg.addFlashAttribute("danger", "Error al eliminar. No se encontró el proveedor.");
+
+        return "redirect:/productos/proveedores/listar";
+    }
+}
---
+++ gestion-limpia/src/main/java/com/burbujas/gestionlimpia/controllers/ReabastecimientoController.java
@@ -0,0 +1,158 @@
+package com.burbujas.gestionlimpia.controllers;
+
+import com.burbujas.gestionlimpia.models.entities.Producto;
+import com.burbujas.gestionlimpia.models.entities.Proveedor;
+import com.burbujas.gestionlimpia.models.entities.Reabastecimiento;
+import com.burbujas.gestionlimpia.models.services.IProductoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/productos/reabastecimientos")
+public class ReabastecimientoController {
+
+    private final IProductoService productoService;
+
+    @Autowired
+    public ReabastecimientoController(IProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+
+    @GetMapping("/listar/{idProducto}")
+    public String listarReabastecimientos(@PathVariable(value = "idProducto") Long idProducto, Model model){
+        model.addAttribute("titulo", "Reabastecimientos");
+
+        List<Reabastecimiento> reabastecimientos = productoService.findAllReabastecimientosByProductoId(idProducto);
+
+        model.addAttribute("reabastecimientos", reabastecimientos);
+
+        return "inventario/reabastecimientos/reabastecimientos";
+    }
+
+    @GetMapping("/listar")
+    public String listarReabastecimientos(Model model){
+        model.addAttribute("titulo", "Reabastecimientos");
+
+        List<Producto> productos = productoService.findAll();
+
+        List<Reabastecimiento> reabastecimientos = new ArrayList<>();
+        productos.forEach(producto -> reabastecimientos.addAll(producto.getReabastecimientos()));
+
+        model.addAttribute("reabastecimientos", reabastecimientos);
+
+        return "inventario/reabastecimientos/reabastecimientos";
+    }
+
+    @GetMapping(value = "/nuevo")
+    public String crearReabastecimiento(Model model) {
+        model.addAttribute("titulo", "Nuevo producto");
+
+        Reabastecimiento reabastecimiento = new Reabastecimiento();
+        List<Producto> productos = productoService.findAll();
+        List<Proveedor> proveedores = productoService.findAllProveedores();
+
+        model.addAttribute("reabastecimiento", reabastecimiento);
+        model.addAttribute("productos", productos); // para el select
+        model.addAttribute("proveedores", proveedores); // para el select
+        model.addAttribute("fechaActual", new Date());
+
+        return "inventario/reabastecimientos/reabastecimiento";
+    }
+
+    @GetMapping(value = "/nuevo/{idProducto}")
+    public String crearReabastecimiento(@PathVariable(value = "idProducto") Long idProducto, RedirectAttributes flashmsg, Model model) {
+        model.addAttribute("titulo", "Nuevo producto");
+
+        Reabastecimiento reabastecimiento = new Reabastecimiento();
+
+        // buscamos y asignamos el producto obtenido por url
+        Producto producto = productoService.findById(idProducto);
+        if(producto != null) {
+            reabastecimiento.setProducto(producto);
+
+            List<Producto> productos = productoService.findAll();
+            List<Proveedor> proveedores = productoService.findAllProveedores();
+
+            model.addAttribute("reabastecimiento", reabastecimiento);
+            model.addAttribute("productos", productos); // para el select
+            model.addAttribute("proveedores", proveedores); // para el select
+            model.addAttribute("fechaActual", new Date());
+
+            return "inventario/reabastecimientos/reabastecimiento";
+        }else{
+            flashmsg.addFlashAttribute("danger", "Error al crear Reabastecimiento. No se encontró el producto.");
+            return "redirect:/productos/inventario";
+        }
+    }
+
+    @GetMapping(value = "/editar/{id}")
+    public String editarReabastecimiento(@PathVariable(value = "id") Long id, RedirectAttributes flashmsg, Model model) {
+
+        Reabastecimiento reabastecimiento = productoService.findReabastecimientoById(id);
+        if (id > 0 && reabastecimiento != null) {
+            List<Producto> productos = productoService.findAll();
+            List<Proveedor> proveedores = productoService.findAllProveedores();
+
+            model.addAttribute("reabastecimiento", reabastecimiento);
+            model.addAttribute("productos", productos); // para el select
+            model.addAttribute("proveedores", proveedores); // para el select
+            model.addAttribute("fechaActual", new Date());
+
+            model.addAttribute("titulo", "Editar reabastecimiento");
+            return "inventario/reabastecimientos/reabastecimiento";
+        } else {
+            flashmsg.addFlashAttribute("danger", "No se encontró el reabastecimiento.");
+            return "redirect:/productos/reabastecimientos/listar";
+        }
+
+    }
+
+    @PostMapping(value = "/guardar")
+    public String guardarReabastecimiento(@Valid Reabastecimiento reabastecimiento, BindingResult result, Model model, RedirectAttributes flashmsg) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", model.getAttribute("titulo"));
+            return "inventario/reabastecimientos/reabastecimiento";
+        }
+        Producto producto = reabastecimiento.getProducto();
+        if(producto != null) {
+            productoService.saveReabastecimiento(reabastecimiento);
+            productoService.addReabastecimiento(producto, reabastecimiento);
+
+            flashmsg.addFlashAttribute("success", "Listado de reabastecimientos actualizado");
+        }else{
+            flashmsg.addFlashAttribute("danger", "Error al guardar el reabastecimiento. Producto no encontrado.");
+        }
+        return "redirect:/productos/reabastecimientos/listar";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarReabastecimiento(@PathVariable(name = "id") Long id, RedirectAttributes flashmsg){
+        Reabastecimiento reabastecimiento = productoService.findReabastecimientoById(id);
+        if (reabastecimiento != null) {
+            Producto producto = reabastecimiento.getProducto();
+            if(producto != null) {
+                productoService.deleteReabastecimiento(producto, reabastecimiento);
+                flashmsg.addFlashAttribute("success", "Reabastecimiento eliminado correctamente.");
+            }else{
+                flashmsg.addFlashAttribute("danger", "Error al eliminar el reabastecimiento. Producto no encontrado.");
+            }
+        }
+        flashmsg.addFlashAttribute("danger", "Error al eliminar. No se encontró el reabastecimiento.");
+
+        return "redirect:/productos/reabastecimientos/listar";
+    }
+}
